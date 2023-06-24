32ea7ef66d7077fb9b0022491803b69bd8c25340
okamoto daiki <ewn3202@gmail.com>
2023-06-25 02:41:57 +0900

    20230625-0230

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -2,14 +2,14 @@
 <html lang="ja">
 
 <head>
-
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Atte</title>
 
     <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}">
     <link rel="stylesheet" href="{{ asset('css/common.css') }}">
-
+    @yield('css')
 </head>
 
 <body>
@@ -34,6 +34,13 @@
         @yield('content')
     </main>
     
+    <footer class="footer">
+        <div class="footer__inner">
+            <div class="footer__content">
+                <p class="footer__item">Atte,inc.</p>
+            </div>
+        </div>
+    </footer>
 </body>
 
 </html>
