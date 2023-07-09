b07b785a095f3dcbb98243831f32e7fe4308a4fb
okamoto daiki <ewn3202@gmail.com>
2023-07-09 21:22:52 +0900

    20230709

--- src/resources/views/stamp.blade.php
+++ src/resources/views/stamp.blade.php
@@ -8,6 +8,34 @@
 @endsection
 
 @section('content')
-    <p>勤怠ボタンのところ</p>
-    <p>{{ Auth::user()->name }} さん、お疲れ様です！</p>
+    <div class="stamp">
+        <div class="stamp__user-message">
+            <p>{{ Auth::user()->name }} さん、お疲れ様です！</p>
+        </div>
+        <div class="works">
+            <form action="" class="works-start" method="POST">
+                @csrf
+                <button class="work-start__button">勤務開始</button>
+            </form>
+            
+            <form action="" class="work-finish" method="POST">
+                @csrf
+                <button class="works-start__button">勤務終了</button>
+            </form>
+        </div>
+
+        <div class="breakings">
+            <form action="" class="break-start" method="POST">
+                @csrf
+                <button class="works-start__button">休憩開始</button>
+            </form>
+            
+            <form action="" class="break-finish" method="POST">
+                @csrf
+                <button class="works-start__button">休憩終了</button>
+            </form>
+
+        </div>
+
+    </div>
 @endsection
